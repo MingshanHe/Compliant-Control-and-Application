8e62b3bc110e96473680bd3ae56443f7ed63c43e
MingshanHe <hemingshan_1999@163.com>
2022-07-27 17:39:05 +0800

    add install dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+ros_distro="noetic"
+
+sudo apt-get install -y ros-${ros_distro}-move-base*
+sudo apt-get install -y ros-${ros_distro}-industrial-robot-status-interface
+sudo apt-get install -y ros-${ros_distro}-rospy-message-converter
+sudo apt-get install -y ros-${ros_distro}-industrial-robot-status-interface
+sudo apt-get install -y ros-${ros_distro}-ur-msgs
+sudo apt-get install -y ros-${ros_distro}-pass-through-controllers
+sudo apt-get install -y ros-${ros_distro}-ur-client-library
